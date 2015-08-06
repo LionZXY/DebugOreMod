674502aa25d1bfca6f1abc5a9397bec2e6bba27e
LionZXY <nikita@kulikof.ru>
2015-08-06 22:06:13 +0300

    First Commit

--- src/main/java/com/lionzxy/debugore/tile/DebugOreTileEntity.java
+++ src/main/java/com/lionzxy/debugore/tile/DebugOreTileEntity.java
@@ -8,6 +8,22 @@ import net.minecraft.tileentity.TileEntity;
  * Created by nikit on 06.08.2015.
  */
 public class DebugOreTileEntity extends TileEntity {
+        /*
+        Crash:
+        [22:02:37] [Server thread/ERROR] [FML]: A TileEntity type com.lionzxy.debugore.tile.DebugOreTileEntity has throw an exception trying to write state. It will not persist. Report this to the mod author
+                java.lang.RuntimeException: class com.lionzxy.debugore.tile.DebugOreTileEntity is missing a mapping! This is a bug!
+	            at net.minecraft.tileentity.TileEntity.writeToNBT(TileEntity.java:96) ~[TileEntity.class:?]
+	            at com.lionzxy.debugore.tile.DebugOreTileEntity.writeToNBT(DebugOreTileEntity.java:23) ~[DebugOreTileEntity.class:?]
+	            at net.minecraft.world.chunk.storage.AnvilChunkLoader.writeChunkToNBT(AnvilChunkLoader.java:395) [AnvilChunkLoader.class:?]
+	            at net.minecraft.world.chunk.storage.AnvilChunkLoader.saveChunk(AnvilChunkLoader.java:204) [AnvilChunkLoader.class:?]
+	            at net.minecraft.world.gen.ChunkProviderServer.safeSaveChunk(ChunkProviderServer.java:287) [ChunkProviderServer.class:?]
+	            at net.minecraft.world.gen.ChunkProviderServer.saveChunks(ChunkProviderServer.java:340) [ChunkProviderServer.class:?]
+	            at net.minecraft.world.WorldServer.saveAllChunks(WorldServer.java:863) [WorldServer.class:?]
+	            at net.minecraft.server.MinecraftServer.saveAllWorlds(MinecraftServer.java:370) [MinecraftServer.class:?]
+	            at net.minecraft.server.integrated.IntegratedServer.tick(IntegratedServer.java:113) [IntegratedServer.class:?]
+	            at net.minecraft.server.MinecraftServer.run(MinecraftServer.java:485) [MinecraftServer.class:?]
+	            at net.minecraft.server.MinecraftServer$2.run(MinecraftServer.java:752) [MinecraftServer$2.class:?]
+         */
 
         private int radius = 0;
         public DebugOreTileEntity(){
